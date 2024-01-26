f2ee173cc593769d5b810817d93fb781a8ef4acf
maxuser74 <maxpasseri@libero.it>
2024-01-27 00:30:30 +0100

    Commit

--- main.py
+++ main.py
@@ -136,7 +136,7 @@ def index():
 
 if __name__ == '__main__':
     if STAT_DEV:
-        app.run(debug=True)
+        app.run(debug=False)
     else:
         from waitress import serve
         serve(app, host="0.0.0.0", port=8080)
